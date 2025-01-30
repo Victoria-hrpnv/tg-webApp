96d713403fd13fd61d19bb01f09f6b1014c6dcba
vikah <vikahrapina00@mail.ru>
2025-01-30 19:35:47 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import {Button} from "antd-mobile";
+import {Button, Avatar} from "antd-mobile";
 
 function App() {
     const [username, setUsername] = useState('');
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const [themeParams, setthemeParams] = useState('');
+    const [themeParams, setThemeParams] = useState('');
+    const [avatar, setAvatar] = useState('');
+
 
 
     useEffect(()=> {
     if (Telegram && Telegram.WebApp) {
         const user = Telegram.WebApp.initDataUnsafe?.user || {};
+        const theme = Telegram.WebApp.themeParams;
+        const avatarStr = Telegram.WebApp.initDataUnsafe?.photo_url;
         setUsername(user.username || 'Неизвестный пользователь');
         setFirstName(user.first_name || '');
         setLastName(user.last_name || '');
-        const theme = Telegram.WebApp.themeParams;
-        setthemeParams(theme)
+        setAvatar(avatarStr);
+        setThemeParams(theme);
+
     }}, []);
 
   return (
       <>
           <pre>{JSON.stringify(themeParams, null, 2)}</pre>
           <p className={'privet'}>Привет, {firstName}</p>
-          <Button>Привет!</Button>
+          <Button className={'button'} >Привет!</Button>
+          <Avatar src={avatar} style={{ '--size': '40px', '--border-radius': '50%'  }} />
       </>
   )
 }
