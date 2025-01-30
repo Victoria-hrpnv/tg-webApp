43d0c7e177a0b34ad8779361255c9cf5c0062404
vikah <vikahrapina00@mail.ru>
2025-01-30 17:25:32 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import {Button} from "antd-mobile";
+
+function App() {
+    const [username, setUsername] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+
+    useEffect(()=> {
+    if (Telegram && Telegram.WebApp) {
+        const user = Telegram.WebApp.initDataUnsafe?.user || {};
+        setUsername(user.username || 'Неизвестный пользователь');
+        setFirstName(user.first_name || '');
+        setLastName(user.last_name || '');
+    }}, []);
+
+  return (
+    <>
+      <Button>Привет!</Button>
+    </>
+  )
+}
+
+export default App
