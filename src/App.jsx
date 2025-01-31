4a03e96629bcf39605fdfdd716abab5e7b7f1ad1
vikah <vikahrapina00@mail.ru>
2025-01-31 14:36:41 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import {Button, Avatar} from "antd-mobile";
+import {Button, DotLoading} from "antd-mobile";
 
 function App() {
-    const [username, setUsername] = useState('');
     const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
     const [themeParams, setThemeParams] = useState('');
-    const [avatar, setAvatar] = useState('');
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(null)
 
 
 
@@ -16,22 +15,50 @@ function App() {
     if (Telegram && Telegram.WebApp) {
         const user = Telegram.WebApp.initDataUnsafe?.user || {};
         const theme = Telegram.WebApp.themeParams;
-        const avatarStr = Telegram.WebApp.initDataUnsafe?.photo_url;
-        setUsername(user.username || 'Неизвестный пользователь');
-        setFirstName(user.first_name || '');
-        setLastName(user.last_name || '');
-        setAvatar(avatarStr);
+        setFirstName(user.first_name || 'false');
         setThemeParams(theme);
-
     }}, []);
+
+    const fetchBtc = async () => {
+        setLoading('true')
+        const apiUrl = "https://api.coinbase.com/v2/prices/BTC-RUB/buy";
+        fetch(apiUrl)
+            .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`Ошибка: ${response.status}`);
+                    }
+                    return response.json();
+                }
+            )
+            .then(json => {
+                setData(json);
+                setLoading('false')
+                console.log(json)
+            })
+            .catch((err)=>{
+                console.log('Ошибка:',err.message)
+            });
+    };
+
+
 
   return (
       <>
           <pre>{JSON.stringify(themeParams, null, 2)}</pre>
-          <p className={'privet'}>Привет, {firstName}</p>
-          <Button className={'button'} >Привет!</Button>
-          <Avatar src={avatar} style={{ '--size': '150px', '--border-radius': '50%'  }} />
-          <p>{avatar}</p>
+          <h1 className={'privet'}>Привет, {firstName}</h1>
+          <h2>Курс BTC</h2>
+          {data ? (
+              <p>{data.data.amount} RUB</p>
+          ) : (
+              loading === 'true' ?  <p>Загрузка..</p> : <p>Нажмите, чтобы узнать</p>
+          )}
+          <Button
+              style={{
+                  backgroundColor: 'var(--tg-theme-button-color)',
+                  color: 'var(--tg-theme-button-text-color)',
+              }}
+              className={'button'}
+              onClick={fetchBtc}>Узнать</Button>
       </>
   )
 }
