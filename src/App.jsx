139928c9778dc4f7dbdbb033c18f658a499c171b
vikah <vikahrapina00@mail.ru>
2025-02-14 17:42:12 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,145 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import {Button} from "antd-mobile";
+import {useState, useEffect} from 'react'
+import {Button, Checkbox, Popup, Space, Tabs, Toast} from "antd-mobile";
 import DiagramOfRadii from "./components/DiagramOfRadii/DiagramOfRadii.jsx";
 
-
-
-
 function App() {
-    const [firstName, setFirstName] = useState('');
     const [themeParams, setThemeParams] = useState('');
-    const [radiusArray, setRadiusArray] = useState(
-        ['1', '0', '1', '0', '1', '0', '1',]
-    );
-    const [labelsArray, setLabelsArray] = useState(
-        ["radius1","radius2","radius3","radius4","radius5","radius6","radius7"]
-    );
-    const [period, setPeriod] = useState(1);
+    const [radiusArray, setRadiusArray] = useState([0, 0, 0, 0, 0, 0, 0]);
+    const [labelsArray, setLabelsArray] = useState(["Утренняя настройка", "Планирование", "Прогулка", "Активный спорт", "Навыки, деньги, статус", "Творчество", "Урок благодарности"]);
+    const [period, setPeriod] = useState(0);
+    const [visible, setVisible] = useState(false);
 
-    useEffect(()=> {
+    useEffect(() => {
         const Telegram = window.Telegram;
         if (Telegram && Telegram.WebApp) {
-            const user = Telegram.WebApp.initDataUnsafe?.user || {};
+            // const user = Telegram.WebApp.initDataUnsafe?.user || {};
             setThemeParams(Telegram.WebApp.themeParams)
-            setFirstName(user.first_name || false);
-            }
-
-        }, []);
-
-    // Функция для загрузки данных с сервера
-    // const fetchData = async (period) => {
-    //     const id = userId || 143937122; // Если userId не определено, используем 143937122
-    //     const url = `http://127.0.0.1:8080/${id}/${period}`;
-    //
-    //     try {
-    //         // Ваш асинхронный код, например, запрос на сервер
-    //         const response = await fetch(url, {
-    //             method: 'GET',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             mode: 'cors',
-    //         });
-    //         // Проверка успешности ответа
-    //         if (!response.ok) {
-    //             throw new Error(`Ошибка при получении данных: ${response.status}`);
-    //         }
-    //         const data = await response.json();
-    //         if (data.message) {
-    //             alert(`Ошибка: ${data.message}`);
-    //             return;
-    //         }
-    //         const labels = Object.keys(data)
-    //         setLabelsArray(labels);
-    //         const radius = Object.values(data)
-    //         setRadiusArray(radius)
-    //
-    //     } catch (error) {
-    //         console.error('Произошла ошибка:', error.message);
-    //     }
-    // };
-
-    //Временные данные
-        const togglePeriod = (period) => {
-            if (period === 1) {
-                setPeriod(1)
-                setLabelsArray(
-                    ["radius1","radius2","radius3","radius4","radius5","radius6","radius7"]
-                );
-                setRadiusArray(
-            ['1', '0', '1', '0', '1', '0', '1',]
-                )
-                } else {
-                setPeriod(7)
-                setLabelsArray(
-                ["radius1","radius2","radius3","radius4","radius5","radius6","radius7"]
-            )
-                setRadiusArray(
-                    ['1', '5', '3', '6', '2', '3', '6',]
-                )}
         }
+        fetchData(0)
+    }, []);
 
-  return (
-      <>
-          <h1 className={'tColor'}>Привет, {firstName}</h1>
-          <div className={'div'}>
-              <DiagramOfRadii theme={themeParams} period = {period} labels = {labelsArray} radius = {radiusArray}></DiagramOfRadii>
-          </div>
-          <h2 className={'tColor'}>Выбери период</h2>
-          <Button
-              style={{
-                  backgroundColor: 'var(--tg-theme-button-color)',
-                  color: 'var(--tg-theme-button-text-color)',
-                  border: 'none',
-                  marginRight: '20px'
-              }}
-              className={'button'}
-              onClick={() => togglePeriod(1)}>День</Button>
-          <Button
-              style={{
-                  backgroundColor: 'var(--tg-theme-button-color)',
-                  color: 'var(--tg-theme-button-text-color)',
-                  border: 'none'
-              }}
-              className={'button'}
-              onClick={() => togglePeriod(30)}>Неделя</Button>
-          {/*<pre>{JSON.stringify(themeParams, null, 2)}</pre>*/}
-      </>
-  )
+    const fetchData = async (period) => {
+        const id = 143937122; // Если userId не определено, используем 143937122
+        const url = `http://192.168.0.16:8080/${id}/${period}`;
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                mode: 'cors',
+            });
+            if (!response.ok) {
+                throw new Error(`Ошибка при получении данных: ${response.status}`);
+            }
+            const data = await response.json();
+            if (data.message) {
+                alert(`Ошибка: ${data.message}`);
+                return;
+            }
+            const labels = Object.keys(data)
+            setLabelsArray(labels);
+            const radius = Object.values(data)
+            setRadiusArray(radius)
+        } catch (error) {
+            console.error('Произошла ошибка:', error.message);
+        }
+    };
+
+    const sendData = async (index) => {
+        const id = 143937122;
+        const url = `http://192.168.0.16:8080/toggle-status`;
+        const radius = labelsArray[index];
+        // console.log(JSON.stringify({radius, userId: id} ))
+        try {
+            const response = await fetch(url,
+                {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({radius, userId: id} ),
+                })
+            if (!response.ok) {
+                throw new Error(`Ошибка: ${response.status}`);
+            }
+            const result = await response.json();
+            console.log("Успешно:", result);
+        }
+        catch (error) {
+            console.error("Ошибка запроса:", error);
+        }
+    }
+
+    const toggleCheckbox = (index) => {
+        setRadiusArray((prev) =>
+            prev.map((item, i) => (i === index ? (item === 0 ? 1 : 0) : item))
+        );
+    };
+
+    return (
+        <>
+            <Tabs onChange={(key) => {
+                const period = parseInt(key, 10);
+                setPeriod(period);
+                fetchData(period);
+            }}>
+                <Tabs.Tab title='Сегодня' key={0}/>
+                <Tabs.Tab title='Неделя' key={7}/>
+                <Tabs.Tab title='Месяц' key={30}/>
+            </Tabs>
+
+            <div className={'div'}>
+                <DiagramOfRadii theme={themeParams} period={period}
+                                labels={labelsArray}
+                                radius={radiusArray}></DiagramOfRadii>
+            </div>
+
+            {period === 0 &&
+                <div className={'popUp__wrapper'}>
+                    <Space align='center'>
+                        <Button className={'button popUp__button'}
+                                onClick={() => {
+                                    setVisible(true)}}>
+                            Закрыть радиус
+                        </Button>
+                    </Space>
+
+                    <Popup
+                           visible={visible}
+                           onMaskClick={() => {
+                               setVisible(false)
+
+                           }}
+                           showCloseButton
+                           onClose={() => {
+                               setVisible(false)
+                              }}>
+                        <div className={'checkListContainer'}>
+                            <Space direction="vertical" block>
+                                {labelsArray.map((label, index) => (
+                                    <Checkbox
+                                        className={'checkbox'}
+                                        key={index}
+                                        checked={radiusArray[index] === 1}
+                                        onClick={() => {
+                                            toggleCheckbox(index);
+                                            sendData(index);
+                                            Toast.show({
+                                                content: 'Сохранено',
+                                                position: "top",
+                                                duration: 700,
+                                            })}}>
+                                        {label}
+                                    </Checkbox>
+                                ))}
+                            </Space>
+                        </div>
+                    </Popup>
+                </div>
+            }
+        </>
+    )
 }
 
 export default App
--- src/components/DiagramOfRadii/DiagramOfRadii.jsx
+++ src/components/DiagramOfRadii/DiagramOfRadii.jsx
@@ -6,29 +6,36 @@ import {
     RadialLinearScale
 } from "chart.js";
 import { PolarArea } from 'react-chartjs-2';
+import {useEffect, useState} from "react";
 ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 
 
-
-
 const DiagramOfRadii = ({theme, labels, radius, period})=>{
+    const [sector, setSector] = useState(null)
+
+    useEffect(() => {
+        setSector(period/3);
+    }, [period]);
+
+    const bColor = () => radius.map((item)=>{
+        if (item < sector){
+            return 'rgba(255, 0, 0, 0.7)';
+        }
+        if (item > sector && item < sector*2 ){
+            return 'rgba(255, 255, 0, 0.8)'
+        }
+        return 'rgba(25, 255, 25, 0.5)';
+    })
 
     const data = {
-        labels: labels, // Подписи сегментов
+        labels: labels,
         datasets: [
             {
-                label: 'My Dataset',
-                data: radius, // Данные
-                backgroundColor: [ // Цвета сегментов
-                    'rgba(255, 99, 132, 0.5)',
-                    'rgba(54, 162, 235, 0.5)',
-                    'rgba(255, 206, 86, 0.5)',
-                    'rgba(75, 192, 192, 0.5)',
-                    'rgba(255, 159, 64, 0.5)',
-                    'rgba(255, 99, 71, 0.5)',
-                    'rgba(153, 102, 255, 0.5)'
-                ],
-                borderWidth: 0,
+                label: 'Значение',
+                data: radius,
+                backgroundColor: period === 0 ? 'rgba(25, 255, 25, 0.5)': bColor(),
+                borderWidth: 1,
+                borderColor:theme.bg_color === "#ffffff" ? 'rgba(0, 0, 0, 0.2)' : 'rgba(255, 255, 255, 0.2)' ,
             }
         ]
     };
@@ -43,35 +50,52 @@ const DiagramOfRadii = ({theme, labels, radius, period})=>{
         },
         scales: {
             r: {
-                max: period,
+                max: period === 0 ? 1 : period,
                 min: 0,
                 pointLabels: {
                     display: true,
                     centerPointLabels: true,
+                    padding: 4,
                     font: {
-                        size: 16
+                        size: 11
+                    } ,
+                    callback: function (label) {
+                        const maxWidth = 10;
+                        const words = label.split(' ');
+                        let currentLine = '';
+                        const lines = [];
+
+                        words.forEach(word => {
+                            if ((currentLine + word).length > maxWidth) {
+                                lines.push(currentLine.trim());
+                                currentLine = '';
+                            }
+                            currentLine += `${word} `;
+                        });
+                        if (currentLine) {
+                            lines.push(currentLine.trim());
+                        }
+                        return lines;
                     }
                 },
                 ticks: {
                     display: false,
-                    stepSize: 1,
-
+                    stepSize: period === 30 ? 5 : 1,
                 },
                 angleLines: {
-                    color: 'rgba(0, 0, 0, 0.1)',
+                    display: true,
+                    color: theme.bg_color === "#ffffff" ? 'rgba(0, 0, 0, 0.4)' : 'rgba(255, 255, 255, 0.4)',
                 },
                 grid: {
-                    color: theme.bg_color === "#ffffff" ? 'rgba(0, 0, 0, 0.5)' : 'rgba(255, 255, 255, 0.5)' ,
+                    color: theme.bg_color === "#ffffff" ? 'rgba(0, 0, 0, 0.4)' : 'rgba(255, 255, 255, 0.4)' ,
                 },
-
             },
         },
     };
 
+
     return( <>
-            <div className={'div'}>
                 <PolarArea data={data} options={options}/>
-            </div>
         </>
     )
 }
